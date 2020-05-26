e3d244f7f398f9142a23fc567b33261c8141c162
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-26 11:26:58 +0530

    UserResponses

---
+++ Websocket with Kafka/src/main/java/com/spring/microservices/model/UserResponse.java
@@ -0,0 +1,17 @@
+package com.spring.microservices.model;
+
+public class UserResponse {
+   
+	String content;
+
+    public UserResponse() {
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public UserResponse(String content) {
+        this.content = content;
+    }
+}
