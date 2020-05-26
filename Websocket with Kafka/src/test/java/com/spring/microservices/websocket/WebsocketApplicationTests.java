0bebb8c5c6c97ba7f02fcbc6c052328f9713f68c
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-26 11:27:21 +0530

    websocket

---
+++ Websocket with Kafka/src/test/java/com/spring/microservices/websocket/WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.microservices.websocket;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
